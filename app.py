5bb0fc2c73e77f43f2dfbb04b431114107437699
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-02-24 19:58:42 +0200

    Added post request for departure time created a html file for depature time

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, jsonify, flash, redirect
 import time
 
 
 
+
 app = Flask(__name__)
+app.secret_key = "hello"
 
 
 @app.route('/')
@@ -11,15 +13,36 @@ def index():
     return render_template('index.html')
 
 
+#def selectPorts():
+    unicornGrid = [[0]*8 for i in range(8)]
 
+    listOfCities = [Aland, Turku, Helsinki, Hamina, Pori, Vaasa, Kokkola, Oulu]
+
+# POST request for departure time
+
+
+@app.route('/departure', methods=["GET", "POST"])
+def foo():
+    if request.method == "POST":
+        error = None
+        departureTime = request.form["departureTime"]
+
+        if not departureTime:
+            error = "Time is required"
+
+        if error is None:
+            print('departure time', departureTime)
+            return render_template("index.html")
+
+        flash(error)
+    return render_template("departure.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
 
-
 # komentorivillä python app.py
-# 
-# netti sivu löytyy: 
+#
+# netti sivu löytyy:
 # http://127.0.0.1:5000/
