b103bb18938f349f35c008ac00603c6eeb143d5a
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-03-10 15:09:41 +0200

    restructured the project

---
+++ auth.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods = ['GET', 'POST'])
+def login():
+    login_error = None
+    if request.method == 'POST':
+        if request.form['password'] == 'Driver':
+            flash('You were successfully logged in as a driver.')
+            return redirect(url_for('set_departure_time'))
+        else:
+            login_error = 'Invalid password.'
+    return render_template('login.html')
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/logout')
+def logout():
+    return render_template('logout.html')
