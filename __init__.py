c86933d82103807e20f21c0d48487d6910647677
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-03-15 12:12:03 +0200

    quick fix

--- __init__.py
+++ __init__.py
@@ -27,3 +27,13 @@ def create_app():
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
