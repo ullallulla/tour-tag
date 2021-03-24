b8ce7bcf576e20d56d2cc91e03f9445c2afdc1f7
JoakimRiistaniemi <79593942+JoakimRiistaniemi@users.noreply.github.com>
2021-03-24 16:01:39 +0200

    Added a commented signup page functionality for account generation purposes

--- auth.py
+++ auth.py
@@ -12,9 +12,6 @@ def login():
     password = request.form.get('password')
     user = User.query.filter_by(name=name).first()
     print(user)
-    #new_user = User(name=name, password=generate_password_hash(password, method='sha256'))       
-    #db.session.add(new_user)
-    #db.session.commit()
     if not user:
         flash('Please input login details.')
         return render_template('login.html')
@@ -22,7 +19,6 @@ def login():
         flash('Incorrect login details.')
         return render_template('login.html')
     login_user(user)
-    #flash('You were successfully logged in.')
     if name == 'Driver':
         return redirect(url_for('main.set_departure_time'))
     if name == 'Leader':
